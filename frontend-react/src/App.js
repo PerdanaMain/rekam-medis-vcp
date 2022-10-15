691a0f78253ef68b50c5c900c7b68b3af9da0ce7
Perdana Main <firman.fp123@gmail.com>
2022-10-15 16:05:58 +0700

    Initialize React for UI

---
+++ frontend-react/src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>I'm From Bootstrap</h2>
+        <button className="btn btn-danger">Click</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
